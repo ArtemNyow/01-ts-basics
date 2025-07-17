5485d9688ae2c123c28ee8c89072e565e2bf8e28
Artem <tem4ik20.1@gmail.com>
2025-07-17 23:37:19 +0300

    add config to vide

--- vide.config.ts
+++ vide.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/01-ts-basics/',
+  base: '/01-ts-basics/',  
+  root: 'src',              
+  build: {
+    outDir: '../dist',      
+    emptyOutDir: true,
+  },
 });
